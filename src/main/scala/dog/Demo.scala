dd61ec78967e3a5cd6767f504f8d3a2e3530b1c1
Ian Daszkowki <i.daszkowski92@gmail.com>
2016-04-26 20:56:40 -0500

    Added test case - needs to be moved

--- src/main/scala/dog/Demo.scala
+++ src/main/scala/dog/Demo.scala
@@ -6,13 +6,38 @@ package dog
 object Demo extends Dog {
 
   def main(args: Array[String]) {
-    bowl1 take()
-    bark("dsds")
-    take
-    2 take()
-
-
+    fetch (1)
+    drop (floor)
+    fetch (2)
+    drop (floor)
+    fetch (3)
+    drop (floor)
+    fetch (4)
+    drop (floor)
+    fetch (5)
+    drop (floor)
+    fetch (6)
+    drop (floor)
+    pickup (floor)
+    safe1 jump ("label")
+    jump ("other label")
+    label "return"
+    bark ("You rolled a ")
+    give
 
     good boy
+  }
+
+  // test case
+  def basic: Unit = {
+    fetch(5)
+    drop (floor)
+    fetch(10)
+    drop (floor)
+    pickup (floor)
+    give
+    bark ("\n")
+    pickup (floor)
+    give
   }
 }
